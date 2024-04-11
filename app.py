8bda707240220c79091f2a54ebb25eff121e16d4
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2024-04-11 10:42:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+#Hamngman Game using Arrays in python [DSA]
+
+import random
+
+#List of words for the game
+
+fruits = ["apple","orange","bnana", "mango", "papaya", "grapes"]
+vegs = ["lady finger", "potato", "Tomato", ]
+
+def choose_word():
+  return random.choice(fruits)
+
+
