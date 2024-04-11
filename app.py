6b3568e173e625c326c773651bdbff9cb8baaf24
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2024-04-11 12:16:19 +0530

    New

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ import random
 
 #List of words for the game
 
-fruits = ["apple","orange","bnana", "mango", "papaya", "grapes"]
-vegs = ["lady finger", "potato", "Tomato", ]
+fruits = ["apple","orange","banana", "mango", "papaya", "grapes"]
+vegs = ["Lady finger", "Potato", "Tomato","Onion" ]
 
 def choose_word():
   return random.choice(fruits)
 
 
 def display_word(word, guessed_letters):
-  
+  display = ""
+  for letter in word:
+    if letter.lower() in guessed_letters:
+      display += letter + ""
+    else:
+      display += "_ "  
+  return display    
+
 
 
 
@@ -25,5 +32,25 @@ def hangman():
   print("Welcome to the Hangman Game!")
   print("Try to guess the word in 5 attempts")
 
-while attempts > 0:
-  print("\n" + display_word(word, guessed_letters))
+  while attempts > 0:
+   print("\n" + display_word(word, guessed_letters))
+   guess = input("Enter a Letter: ").lower()
+
+   if guess in guessed_letters:
+     print("You already guessed that letter")
+
+   elif guess in word:
+     guessed_letters.append(guess)
+   if all(letter in guessed_letters for letter in word):
+       print("Congrats! you guessed right", word)
+       break  
+     
+   else:
+     attempts -= 1
+     print("Incorrect guess. You have", attempts, "attempts remaining." ) 
+
+  if attempts == 0:
+     print("Sorry, you have zero attempts left. The word was:", word) 
+
+
+hangman()     
