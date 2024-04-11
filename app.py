a24ac1bd48de6c9ce8b4a18cfb66f7382a8f1907
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2024-04-11 12:13:59 +0530

    Updated

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ def choose_word():
   return random.choice(fruits)
 
 
+def display_word(word, guessed_letters):
+  
+
+
+
+
+def hangman():
+  word = choose_word()
+  guessed_letters = []
+  attempts = 5
+
+  print("Welcome to the Hangman Game!")
+  print("Try to guess the word in 5 attempts")
+
+while attempts > 0:
+  print("\n" + display_word(word, guessed_letters))
